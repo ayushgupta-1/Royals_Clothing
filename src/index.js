98e1eb8190b5e96ecf62e3f2416f9e996db5489d
Ayush Gupta <ayushfundaa99@gmail.com>
2022-04-01 00:35:53 +0530

    finished auth page with authListener

--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.scss";
+
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
+import { UserProvider } from "./contexts/user.context";
+
+import "./index.scss";
+
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <UserProvider>
+        <App />
+      </UserProvider>
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById("root")
